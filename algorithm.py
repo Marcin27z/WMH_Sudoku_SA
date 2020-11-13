b84de0a9d18bda109fd2c2f4ea7e9935f51f0dcd
Marcin Żyżyński <marcin.zyzynski@gmail.com>
2020-11-13 21:32:25 +0100

    added alternate cost function and neighbour creation

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,34 @@
+from random import randint
+
 from full_board import FullBoard
 from sudoku_solution import SudokuSolution
 
 
 def algorithm():
     full_board = FullBoard()
-    full_board.reduce_fields(15)
-    sudoku_solution = SudokuSolution(full_board.grid)
-    print(sudoku_solution)
-    print()
-    # print(sudoku_solution.create_neighbour())
+    full_board.reduce_fields(60)
+    sudoku_solution = SudokuSolution.new(full_board.grid, False)
+
+    mode = "random"
+
+    if mode == "not random":
+        sudoku_solution.fill_random()
+    else:
+        sudoku_solution.fill_regions()
 
     cost = sudoku_solution.cost()
     i = 0
     while cost != 0:
-        neighbour = sudoku_solution.create_neighbour()
-        # print(sudoku_solution)
-        # print(sudoku_solution.difference(neighbour))
-        if i % 10 == 0:
+        if mode == "random":
+            neighbour = sudoku_solution.create_neighbour()
+        else:
+            neighbour = sudoku_solution.create_neighbour_2()
+        print(cost)
+        if i % 100 == 0:
             print(cost)
         i += 1
-        if neighbour.cost() < sudoku_solution.cost():
+        if neighbour.cost() < sudoku_solution.cost() or randint(0, 1000) < 5:
             sudoku_solution = neighbour
+            cost = neighbour.cost()
 
     print(sudoku_solution)
--- sudoku_solution.py
+++ sudoku_solution.py
@@ -1,5 +1,12 @@
 from __future__ import annotations
 
+import random
+
+import numpy as np
+
+import math
+
+from grid import Grid
 from sudoku_grid import SudokuGrid
 from random import sample
 
@@ -8,13 +15,35 @@ class SudokuSolution(SudokuGrid):
 
     def __init__(self, sudoku_grid):
         super().__init__(sudoku_grid)
-        self.available = [(i, j) for i in range(9) for j in range(9) if self.grid[i][j] == 0]
-        self.counts = [9 for _ in range(10)]
-        for _, row in enumerate(self.grid):
+        self.available = []
+        self.counts = []
+        self.conflicts = []
+        self.empty_in_regions = []
+
+    @classmethod
+    def new(cls, sudoku_grid, fill=False):
+        n = cls(sudoku_grid)
+        n.available = [(i, j) for i in range(9) for j in range(9) if n.grid[i][j] == 0]
+        if len(n.available) == 1:
+            print("cos nie dziala")
+        n.counts = [9 for _ in range(10)]
+        for _, row in enumerate(n.grid):
             for _, elem in enumerate(row):
-                self.counts[elem] -= 1
-        self.counts = self.counts[1:]
-        self.fill_random()
+                n.counts[elem] -= 1
+        n.counts = n.counts[1:]
+        if fill:
+            n.fill_random()
+            print("filling")
+        return n
+
+    @classmethod
+    def copy(cls, sudoku_solution: SudokuSolution):
+        cp = cls(SudokuSolution.copy_grid(sudoku_solution.grid))
+        cp.available = sudoku_solution.available
+        cp.conflicts = cp.conflicts
+        cp.counts = sudoku_solution.counts
+        cp.empty_in_regions = sudoku_solution.empty_in_regions
+        return cp
 
     def __str__(self) -> str:
         return "\n".join([" ".join(map(lambda x: str(x), self.grid[i])) for i in range(9)])
@@ -23,6 +52,21 @@ class SudokuSolution(SudokuGrid):
         available_numbers_set = [i + 1 for i in range(len(self.counts)) for j in range(self.counts[i])]
         for (i, j), number in zip(self.available, available_numbers_set):
             self.grid[i][j] = number
+        self.conflicts = self.calculate_conflicts()
+
+    def fill_regions(self):
+        not_available_in_regions = []
+        for i in range(3):
+            for j in range(3):
+                not_available_in_regions.append({self.grid[i * 3 + k][j * 3 + l] for k in range(3) for l in range(3) if self.grid[i * 3 + k][j * 3 + l] != 0})
+                self.empty_in_regions.append([(i * 3 + k, j * 3 + l) for k in range(3) for l in range(3) if self.grid[i * 3 + k][j * 3 + l] == 0])
+
+        regions = [{j for j in range(1, 10)} for _ in range(9)]
+        available_in_regions = [regions[i].difference(not_available_in_regions[i]) for i in range(9)]
+        for i in range(9):
+            for (x, y), number in zip(self.empty_in_regions[i], available_in_regions[i]):
+                self.grid[x][y] = number
+        self.conflicts = self.calculate_conflicts()
 
         # sprawdzenie, czy faktycznie każda cyfra występuje 9 razy
         # counts = [0 for _ in range(9)]
@@ -31,24 +75,69 @@ class SudokuSolution(SudokuGrid):
         #         counts[elem - 1] += 1
         # print(counts)
 
+    def calculate_conflicts(self) -> []:
+        def duplicates_in_region(row: int, column: int, number: int) -> int:
+            row_region_id = (row % 9 // 3) * 3
+            column_region_id = (column % 9 // 3) * 3
+            column_region_range = range(column_region_id, column_region_id + 3)
+            row_region_range = range(row_region_id, row_region_id + 3)
+            return sum([self.grid[i][j] == number for i in row_region_range for j in column_region_range]) - 1
+
+        def duplicates_in_column(column: int, number: int) -> int:
+            return sum([self.grid[i][column] == number for i in range(9)]) - 1
+
+        def duplicates_in_row(row: int, number: int) -> int:
+            return sum([self.grid[row][i] == number for i in range(9)]) - 1
+
+        conflicts = [duplicates_in_row(i, self.get(i, j)) + duplicates_in_column(j, self.get(i, j)) + duplicates_in_region(i, j, self.get(i, j)) for (i, j) in self.available]
+        return conflicts
+
     def swap(self, x1, y1, x2, y2):
         self.put(x1, y1, self.get(x1, y1) + self.get(x2, y2))
         self.put(x2, y2, self.get(x1, y1) - self.get(x2, y2))
         self.put(x1, y1, self.get(x1, y1) - self.get(x2, y2))
+        self.conflicts = self.calculate_conflicts()
+        if self.get(x1, y1) == 0 or self.get(x2, y2) == 0:
+            print("tutaj")
 
     def create_neighbour(self) -> SudokuSolution:
-        candidates = sample(self.available, 2)
+        neighbour = SudokuSolution.copy(self)
+
+        probabilities = list(map(lambda x: 1 - math.exp(-x), self.conflicts))
+        sum_of_probabilities = sum(probabilities)
+        probabilities = list(map(lambda x: x / sum_of_probabilities, probabilities))
+        candidates = np.random.choice(range(len(self.available)), replace=False, p=probabilities, size=2)
+        x1 = self.available[candidates[0]][0]
+        y1 = self.available[candidates[0]][1]
+        x2 = self.available[candidates[1]][0]
+        y2 = self.available[candidates[1]][1]
+
+        neighbour.swap(x1, y1, x2, y2)
+        return neighbour
+
+    def create_neighbour_2(self) -> SudokuSolution:
+        neighbour = SudokuSolution.copy(self)
+
+        empty = self.empty_in_regions[:]
+        random.shuffle(empty)
+        empty = list(filter(lambda x: len(x) >= 2, empty))
+        if len(empty) == 0:
+            return neighbour
+        candidates = empty[0]
         x1 = candidates[0][0]
         y1 = candidates[0][1]
         x2 = candidates[1][0]
         y2 = candidates[1][1]
-        # print(candidates)
-        neighbour = SudokuSolution(SudokuGrid.copy_grid(self.grid))
-        neighbour.available = self.available
+
         neighbour.swap(x1, y1, x2, y2)
         return neighbour
 
-    def cost(self):
+    def cost(self) -> int:
+        if not self.conflicts:
+            self.conflicts = self.calculate_conflicts()
+        return sum(self.conflicts)
+
+    def cost2(self) -> int:
         total_cost = 0
         for i, row in enumerate(self.grid):
             for j, elem in enumerate(row):
