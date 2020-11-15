2778460e7f88f49d0b435a28cd1a0ed42617767e
Kuba <kubag0125@gmail.com>
2020-11-15 19:10:21 +0100

    Dodanie zmian do algorytmu(uwzględnienie temperatury i wpływu na wybór lepszego rozwiązania)

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,15 @@
-from random import randint
+from random import uniform
+from math import exp
 
 from full_board import FullBoard
 from sudoku_solution import SudokuSolution
 
 
 def algorithm():
-    # full_board = FullBoard()
-    # full_board.reduce_fields(15)
-    full_board = FullBoard.from_cache(67, 0)
-    sudoku_solution = SudokuSolution.new(full_board.grid, False)
+    full_board = FullBoard()
+    full_board.reduce_fields(20)
+    #full_board = FullBoard.from_cache(67, 0)
+    sudoku_solution = SudokuSolution.new(full_board.grid, True)
 
     mode = "random"
 
@@ -19,7 +20,10 @@ def algorithm():
 
     cost = sudoku_solution.cost()
     i = 0
-    while cost != 0:
+    step = 0
+    temperature = 0.99
+
+    while cost > 0:
         if mode == "random":
             neighbour = sudoku_solution.create_neighbour()
         else:
@@ -28,8 +32,14 @@ def algorithm():
         if i % 100 == 0:
             print(cost)
         i += 1
-        if neighbour.cost() < sudoku_solution.cost() or randint(0, 1000) < 5:
+        step += 1
+        #zmiana temperatury co pewien krok lub bez pętli w każdym kroku
+        if step == 2:
+            temperature = uniform(0, 1) * temperature
+            step = 0
+        if neighbour.cost() < sudoku_solution.cost() or uniform(0,1) < exp(-(neighbour.cost()-sudoku_solution.cost())/temperature):
             sudoku_solution = neighbour
             cost = neighbour.cost()
 
+    print('Liczba iteracji:' + str(i))
     print(sudoku_solution)
